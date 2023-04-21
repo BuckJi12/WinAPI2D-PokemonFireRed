d1e1259412e2a284329a6ff42ab0baad96b28d10
BuckJi12 <killa5294@gmail.com>
2023-04-21 20:39:04 +0900

    refactor: CMove 초기화
    CMove의 멤버 함수들을 초기화하였다

--- WinAPI/CMove.cpp
+++ WinAPI/CMove.cpp
@@ -3,6 +3,18 @@
 
 CMove::CMove()
 {
+	// 기술 정보
+	m_moveStat.name = L"";
+	m_moveStat.description = L"";
+	m_moveStat.maxPP = 35;
+	m_moveStat.curPP = 35;
+	m_moveStat.moveDamage = 1;
+
+	// 기술 타입
+	m_moveType = PokemonType::Normal;
+
+	// 기술 종류
+	m_moveKind = MoveKind::Physics;
 }
 
 CMove::~CMove()
