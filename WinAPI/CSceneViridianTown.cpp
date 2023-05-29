9e0a12a75b412757d4f268ea4a04fdf3f4c216b2
BuckJi12 <killa5294@gmail.com>
2023-05-30 00:31:21 +0900

    feat: CSceneViridianTown 구현
    상록시티의 포탈들과 타일들을 설치하였다

--- WinAPI/CSceneViridianTown.cpp
+++ WinAPI/CSceneViridianTown.cpp
@@ -1,6 +1,9 @@
 #include "framework.h"
 #include "CSceneViridianTown.h"
 
+#include "CPlayer.h"
+#include "CWarp.h"
+
 CSceneViridianTown::CSceneViridianTown()
 {
 	m_pImageBackGround	= nullptr;
@@ -18,12 +21,32 @@ void CSceneViridianTown::Init()
 	m_pImageBackGround = new CImageObject;
 	m_pImageBackGround->SetImage(RESOURCE->LoadImg(L"ViridianTown", L"Image\\Map\\ViridianTown.png"));
 	m_pImageBackGround->SetPos(0,0);
-	m_pImageBackGround->SetLayer(Layer::Environment);
 	AddGameObject(m_pImageBackGround);
+
+	m_pPlayer = new CPlayer;
+	m_pPlayer->SetPos(1533, 2480);
+	m_pPlayer->ChangeDir(PlayerDir::Up);
+	AddGameObject(m_pPlayer);
+
+	m_pWarp = new CWarp;
+	m_pWarp->SetDestination(GroupScene::Route01); // 포켓몬센터
+	m_pWarp->SetPos(1697, 1690);
+	m_pWarp->SetScale(56, 25);
+	AddGameObject(m_pWarp);
+
+	m_pWarp2 = new CWarp;
+	m_pWarp2->SetDestination(GroupScene::Route01);
+	m_pWarp2->SetPos(1530, 2550);
+	m_pWarp2->SetScale(270, 34);
+	AddGameObject(m_pWarp2);
+
+	LoadTile(GETPATH + L"Tile\\ViridianTown.tile");
 }
 
 void CSceneViridianTown::Enter()
 {
+	CAMERA->FadeIn(0.5f);
+	CAMERA->SetTargetObj(m_pPlayer);
 }
 
 void CSceneViridianTown::Update()
@@ -40,4 +63,8 @@ void CSceneViridianTown::Exit()
 
 void CSceneViridianTown::Release()
 {
+	DELETEOBJECT(m_pImageBackGround);
+	DELETEOBJECT(m_pPlayer);
+	DELETEOBJECT(m_pWarp);
+	DELETEOBJECT(m_pWarp2);
 }
